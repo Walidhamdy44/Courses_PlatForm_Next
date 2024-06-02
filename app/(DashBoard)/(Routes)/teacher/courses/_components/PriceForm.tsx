5ad2f6caf8a196061cee6717c0eb0f2d29ba2d32
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-03 02:59:32 +0300

    u

---
+++ app/(DashBoard)/(Routes)/teacher/courses/_components/PriceForm.tsx
@@ -0,0 +1,128 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { Course } from "@prisma/client";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import toast from "react-hot-toast";
+import { Badge } from "@/components/ui/badge";
+import { Edit, X } from "lucide-react";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+
+interface PriceForm {
+  initialData: Course;
+  courseId: string;
+}
+
+const formSchema = z.object({
+  price: z.string().min(2, {
+    message: "Price of Course is Required.",
+  }),
+});
+
+const PriceForm = ({ courseId, initialData }: PriceForm) => {
+  const router = useRouter();
+
+  const [allowed, setAllowed] = useState(false);
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { price: initialData.price || undefined },
+  });
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, {
+        price: parseFloat(values.price),
+      });
+      toast.success("Course price Updated Successfully!");
+      router.refresh();
+      setAllowed(false);
+    } catch (error) {
+      toast.error("Something went wrong!");
+    }
+  };
+
+  return (
+    <div className="bg-slate-100 p-[15px]  rounded-md shadow-sm mt-6 select-none">
+      <div className="flex items-center gap-3 bg-slate-100 justify-between">
+        <span className="text-[19px]">Course price</span>
+        {allowed ? (
+          <Badge
+            className="cursor-pointer"
+            onClick={() => {
+              setAllowed(!allowed);
+            }}
+          >
+            <X />
+          </Badge>
+        ) : (
+          <Badge
+            className="cursor-pointer"
+            onClick={() => {
+              setAllowed(!allowed);
+            }}
+          >
+            <Edit />
+          </Badge>
+        )}
+      </div>
+      {!allowed ? (
+        <div className="pt-[20px] text-[16px] text-gray-700">
+          {initialData.price ? (
+            <p className="text-rose-500 font-bold text-[20px]">
+              {initialData.price} $
+            </p>
+          ) : (
+            <p className="text-gray-400"> No price yet </p>
+          )}
+        </div>
+      ) : null}
+      {allowed ? (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      step="0.01"
+                      placeholder=" e.g '200 '"
+                      {...field}
+                      disabled={isSubmitting}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center ">
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Update ⚡
+              </Button>
+            </div>
+          </form>
+        </Form>
+      ) : null}
+    </div>
+  );
+};
+
+export default PriceForm;
