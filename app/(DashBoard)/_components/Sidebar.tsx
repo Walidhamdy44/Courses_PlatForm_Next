aedb46ceb12fd8d019ce87e85d6cb3fbccee1fc9
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-04 02:51:05 +0300

    update

--- app/(DashBoard)/_components/Sidebar.tsx
+++ app/(DashBoard)/_components/Sidebar.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-
 import SideLinks from "./SideLinks";
+import Logo from "@/app/_components-home/Logo";
 
 const Sidebar = () => {
   return (
     <div className="flex items-start gap-4 bg-white border-r flex-col shadow-md h-full">
-      <div className="cursor-pointer text-center rounded-2xl overflow-hidden flex items-center gap-3 p-[10px]">
-        <Image src="/imgs/logo.jpg" alt="Company Logo" width={50} height={50} />
-        <span className=" font-extrabold">Courses Platform</span>
+      <div className="mx-auto mt-[20px] mb-0">
+        <Logo />
       </div>
       <SideLinks />
     </div>
