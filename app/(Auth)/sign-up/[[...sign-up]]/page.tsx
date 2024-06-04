054ae545f7b9ffb5617a87c916b3934b0a27cebe
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-04 19:50:45 +0300

    i

--- app/(Auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(Auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,8 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
-      <SignUp />;
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+      <SignUp />
     </div>
   );
 }
