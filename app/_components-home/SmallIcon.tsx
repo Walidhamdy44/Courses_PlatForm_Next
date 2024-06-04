42a8cd254997ac5c3d74ff07d6e6506ff0d288b7
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-04 19:51:49 +0300

    new update

---
+++ app/_components-home/SmallIcon.tsx
@@ -0,0 +1,11 @@
+import { VideoIcon } from "lucide-react";
+
+const SmallIcon = () => {
+  return (
+    <div className="icon-vedio absolute bg-green-400">
+      <VideoIcon />
+    </div>
+  );
+};
+
+export default SmallIcon;
