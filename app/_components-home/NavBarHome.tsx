715bd1715739309f493e11524f970d86e4d328d0
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-04 19:51:06 +0300

    updated

--- app/_components-home/NavBarHome.tsx
+++ app/_components-home/NavBarHome.tsx
@@ -1,17 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { MonitorPlay } from "lucide-react";
 import Logo from "./Logo";
+import BtnsHome from "./BtnsHome";
 
 const NavBarHome = () => {
   return (
-    <div className="flex items-center gap-4 justify-between p-4 bg-white shadow-md w-full h-[80px] fixed">
+    <div className="container flex items-center gap-4 justify-between z-20 bg-white shadow-md w-full h-[80px] fixed">
       <Logo />
-      <div className="flex items-center gap-4 ">
-        <Button variant="outline" className="rounded-2xl border-green-700">
-          Sign Up
-        </Button>
-        <Button className="rounded-2xl border-green-700">Sign In</Button>
-      </div>
+      <BtnsHome />
     </div>
   );
 };
