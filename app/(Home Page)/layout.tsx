a023a52773bd963068f1f9a555070a94aebf5264
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-12 11:49:01 +0300

    updte

---
+++ app/(Home Page)/layout.tsx
@@ -0,0 +1,18 @@
+import "../globals.css";
+
+import Footer from "@/components/Footer";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
