906035e306d36b0cf5e2e455e706afd20b350b9a
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-19 02:03:33 +0300

    ute

---
+++ app/api/courses/[id]/chapter/[chapterId]/route.ts
@@ -0,0 +1,41 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export const DELETE = async (
+  req: Request,
+  { params }: { params: { id: string; chapterId: String } }
+) => {
+  try {
+    const { userId } = auth();
+
+    const userOwner = await db.course.findUnique({
+      where: { id: params.id, userId },
+    });
+
+    if (!userOwner) {
+      return new NextResponse("Unauthorized", {
+        status: 401,
+      });
+    }
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", {
+        status: 401,
+      });
+    }
+
+    const chapter = await db.chapter.delete({
+      where: {
+        id: params.chapterId,
+        courseId: params.id,
+      },
+    });
+
+    return NextResponse.json(chapter);
+  } catch (err) {
+    return new NextResponse("Internal Error", {
+      status: 500,
+    });
+  }
+};
