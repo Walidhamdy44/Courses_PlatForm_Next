e243b065b353f06481d39c9454820c155b93d463
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-19 11:04:43 +0300

    title chapter route handler

--- app/api/courses/[id]/chapter/[chapterId]/route.ts
+++ app/api/courses/[id]/chapter/[chapterId]/route.ts
@@ -39,3 +39,42 @@ export const DELETE = async (
     });
   }
 };
+
+export const PATCH = async (
+  req: Request,
+  { params }: { params: { id: string; chapterId: String } }
+) => {
+  try {
+    const { userId } = auth();
+    const values = await req.json();
+    const userOwner = await db.course.findUnique({
+      where: { id: params.id, userId },
+    });
+
+    if (!userOwner) {
+      return new NextResponse("Unauthorized", {
+        status: 401,
+      });
+    }
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", {
+        status: 401,
+      });
+    }
+
+    const chapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.id,
+      },
+      data: { ...values },
+    });
+
+    return NextResponse.json(chapter);
+  } catch (err) {
+    return new NextResponse("Internal Error", {
+      status: 500,
+    });
+  }
+};
