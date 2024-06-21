6a15cd9740af053155c18f6bb352c5597bafad70
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-21 16:28:37 +0300

    last update

--- app/(DashBoard)/(Routes)/teacher/courses/_table/columns.tsx
+++ app/(DashBoard)/(Routes)/teacher/courses/_table/columns.tsx
@@ -1,19 +1,107 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Course } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
 
 export const columns: ColumnDef<Course>[] = [
   {
     accessorKey: "title",
-    header: "Title",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Title
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "price",
-    header: "Price",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Price
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <Badge
+          className={cn(
+            `bg-cyan-600`,
+            row.getValue("price") === 0 && "bg-green-400"
+          )}
+        >
+          {row.getValue("price") === 0 ? "Free" : "$" + row.getValue("price")}
+        </Badge>
+      );
+    },
   },
   {
     accessorKey: "isPublished",
-    header: "Published",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Published
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const isPublished = row.getValue("isPublished") || false;
+      return (
+        <Badge className={cn(`bg-slate-500`, isPublished && "bg-green-400")}>
+          {isPublished ? "Published" : "Draft"}
+        </Badge>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const { id } = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <MoreHorizontal className="w-4 h-4" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>
+              <Link
+                className="flex items-center gap-2"
+                href={`/teacher/courses/${id}`}
+              >
+                Edit
+                <Pencil className="h-4" />
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+          <DropdownMenuSeparator />
+        </DropdownMenu>
+      );
+    },
   },
 ];
