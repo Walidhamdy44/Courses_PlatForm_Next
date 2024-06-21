3ea3252b5af0367a0723744484c4a6b0ce1adb72
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-21 04:50:17 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import ToastProvider from "@/components/Providers/ToastProvider";
+import { ConfettiProvider } from "@/components/Providers/confettiprovider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ConfettiProvider />
           <ToastProvider />
           {children}
         </body>
