b6871551c8398f558e7308f4144c6343a0467ba5
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-22 00:28:22 +0300

    update

--- app/(DashBoard)/(Routes)/explore/page.tsx
+++ app/(DashBoard)/(Routes)/explore/page.tsx
@@ -1,5 +1,21 @@
-const ExplorePage = () => {
-  return <div></div>;
+import { db } from "@/lib/db";
+import Categories from "./_components/Categories";
+import SearchNavBar from "../../_components/SearchNavBar";
+
+const ExplorePage = async () => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+  return (
+    <div className="p-6">
+      <div className="w-[95%] block md:hidden mb-[30px]">
+        <SearchNavBar />
+      </div>
+      <Categories items={categories} />
+    </div>
+  );
 };
 
 export default ExplorePage;
