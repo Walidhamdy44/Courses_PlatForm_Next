fdcf92fd67e21e91afe97a57f9116b6a018e508c
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-26 02:47:09 +0300

    v

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,52 @@
+import Stripe from "stripe";
+import { headers } from "next/headers";
+import { stripe } from "@/lib/stripe";
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  const signature = (await headers().get("Stripe-Signature")) as string;
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_KEY!
+    );
+  } catch (e) {
+    console.log("Error constructing webhook event:", e);
+    return new NextResponse("Error", {
+      status: 400,
+    });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+
+  const userId = session?.metadata?.userId;
+  const courseId = session?.metadata?.courseId;
+
+  if (event.type === "checkout.session.completed") {
+    if (!userId || !courseId) {
+      return new NextResponse("Error", {
+        status: 400,
+      });
+    }
+
+    await db.purchase.create({
+      data: {
+        courseId,
+        userId,
+      },
+    });
+
+    return new NextResponse(null, { status: 200 });
+  } else {
+    console.log("Unhandled event type:", event.type);
+    return new NextResponse("Unhandled event type", {
+      status: 200,
+    });
+  }
+}
