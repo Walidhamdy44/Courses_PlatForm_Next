0e318ba09b4870cf33f3fe16f54b1dad0689d95b
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-25 22:28:05 +0300

    update

--- app/(course)/courses/[courseId]/page.tsx
+++ app/(course)/courses/[courseId]/page.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-const page = () => {
-  return <div>lkk</div>;
+const page = async ({
+  params,
+}: {
+  params: {
+    courseId: string;
+  };
+}) => {
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapter: {
+        where: {
+          isPublished: true,
+        },
+      },
+    },
+  });
+  if (!course) {
+    return redirect("/");
+  }
+  return redirect(`/courses/${course.id}/chapter/${course.chapter[0].id}`);
 };
 
 export default page;
