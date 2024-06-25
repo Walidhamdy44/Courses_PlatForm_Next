d74702a1b4be038e7ac5a01d64193c6b5f09901a
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-25 22:27:11 +0300

    update

--- app/(course)/courses/_components/CoursesMenuMobile.tsx
+++ app/(course)/courses/_components/CoursesMenuMobile.tsx
@@ -1,8 +1,12 @@
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import CourseSidebar from "./CourseSidebar";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
 
 const CoursesMenuMobile = () => {
+  const { userId } = auth();
+
   return (
     <Sheet>
       <SheetTrigger className=" hover:text-green-500 transition text-[18px]">
