1cf172223ff609936c9660b0bc66372b5cfc3c15
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-25 03:17:33 +0300

    update

--- app/(DashBoard)/(Routes)/explore/page.tsx
+++ app/(DashBoard)/(Routes)/explore/page.tsx
@@ -1,12 +1,34 @@
 import { db } from "@/lib/db";
 import Categories from "./_components/Categories";
 import SearchNavBar from "../../_components/SearchNavBar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { getCourse } from "@/actions/get-courses";
+import CourseCard from "./_components/CourseCard";
+import NoCourses from "./_components/NoCourses";
 
-const ExplorePage = async () => {
+interface searchParams {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const ExplorePage = async ({ searchParams }: searchParams) => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
+  });
+
+  const courses = await getCourse({
+    userId,
+    ...searchParams,
   });
   return (
     <div className="p-6">
@@ -14,6 +36,18 @@ const ExplorePage = async () => {
         <SearchNavBar />
       </div>
       <Categories items={categories} />
+      <div className="grid mt-[30px] gap-x-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {courses.map((course) => (
+          <div key={course.id}>
+            <CourseCard
+              course={course}
+              nChapters={course.chapter.length}
+              cat={course.category.name}
+            />
+          </div>
+        ))}
+      </div>
+      {courses.length === 0 && <NoCourses />}
     </div>
   );
 };
