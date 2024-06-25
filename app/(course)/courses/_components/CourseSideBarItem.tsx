dc190f1d2d499c225a8e836c54fec60fe920b4ee
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-25 22:27:54 +0300

    update

--- app/(course)/courses/_components/CourseSideBarItem.tsx
+++ app/(course)/courses/_components/CourseSideBarItem.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { CheckCircle, FileVideo } from "lucide-react";
+import { CheckCircle, FileVideo, PlayCircle } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 
 const CourseSideBarItem = ({ cha, courseId }: any) => {
@@ -9,14 +9,13 @@ const CourseSideBarItem = ({ cha, courseId }: any) => {
   const router = useRouter();
 
   const isActive = pathName.includes(cha.id);
-
   const onClick = () => {
     router.push(`/courses/${courseId}/chapter/${cha.id}`);
   };
   return (
     <div
       className={cn(
-        "bg-white cursor-pointer border-b-2 border-gray-400 w-full text-gray-500  pl-[20px] py-[20px] text-center flex items-center gap-6 hover:text-green-500 hover:bg-gray-200 transition",
+        "bg-white cursor-pointer border-b-2 border-gray-200 w-full text-gray-500  pl-[20px] py-[20px] text-center flex items-center gap-6 hover:text-green-500 hover:bg-gray-200 transition",
         cha.isCompleted && "bg-gray-200 text-green-500",
         isActive && "bg-green-100"
       )}
@@ -24,6 +23,8 @@ const CourseSideBarItem = ({ cha, courseId }: any) => {
     >
       {cha.isCompleted ? (
         <CheckCircle className="w-6 h-6 text-green-500" />
+      ) : isActive ? (
+        <PlayCircle className="w-6 h-6 text-gray-500" />
       ) : (
         <FileVideo className="w-6 h-6 text-gray-500" />
       )}
