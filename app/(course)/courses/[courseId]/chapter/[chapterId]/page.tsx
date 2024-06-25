936cfebb4f74c9f949ee0c0570477b745c0f7a6d
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-25 22:27:22 +0300

    update

--- app/(course)/courses/[courseId]/chapter/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapter/[chapterId]/page.tsx
@@ -1,5 +1,68 @@
-const page = () => {
-  return <div>Page</div>;
+import Banner from "@/components/Banner";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import VideoPlayer from "../../../_components/VideoPlayer";
+import ChapterCompleteSec from "../../../_components/ChapterCompleteSec";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: {
+    courseId: string;
+    chapterId: string;
+  };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const chapter = await db.chapter.findUnique({
+    where: {
+      id: params.chapterId,
+      isPublished: true,
+    },
+  });
+
+  //
+
+  const attachments = await db.attachment.findMany({
+    where: {
+      courseId: params.courseId,
+    },
+  });
+
+  //
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+      isPublished: true,
+    },
+    include: {
+      purchase: true,
+    },
+  });
+
+  //
+  return (
+    <div>
+      {!chapter.ifFree ? (
+        <Banner message="⚠️      This Chapter is Locked!" />
+      ) : null}
+      <div className="flex flex-col gap-3 py-[30px]">
+        <VideoPlayer vidUrl={chapter?.videoUrl!} isFree={chapter?.ifFree!} />
+        <ChapterCompleteSec
+          title={chapter?.chapterTitle!}
+          desc={chapter?.description!}
+          complete={chapter?.isCompleted!}
+          purchase={course?.purchase!}
+          attachments={attachments}
+        />
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default ChapterIdPage;
