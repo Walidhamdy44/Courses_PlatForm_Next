f0e78983c9c9e532ce736c94f40b2b942d4efd99
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-28 01:34:37 +0300

    update layout

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -1,8 +1,9 @@
 import { auth } from "@clerk/nextjs/server";
-import CourseNavBar from "../_components/CourseNavBar";
-import CourseSidebar from "../_components/CourseSidebar";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
+import CourseSidebar from "./_components/CourseSidebar";
+import CourseNavBar from "./_components/CourseNavBar";
+import CoursesMenuMobile from "./_components/CoursesMenuMobile";
 
 const layout = async ({
   children,
@@ -40,15 +41,10 @@ const layout = async ({
   return (
     <div>
       <div className=" md:pl-80 h-[80px] w-full flex fixed inset-y-0 z-40 bg-white">
-        <CourseNavBar />
+        <CourseNavBar course={course} />
       </div>
       <div className="hidden md:flex w-80 h-full flex-col fixed inset-y-0 z-40">
-        <CourseSidebar
-          course={course}
-          title={course.title}
-          category={course.category.name!}
-          chapters={course.chapter}
-        />
+        <CourseSidebar course={course} />
       </div>
       <main className="md:pl-80 w-full pt-[80px]">{children}</main>
     </div>
