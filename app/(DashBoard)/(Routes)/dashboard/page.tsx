a96c27abd15148c3bc5085190df3953c8d4fb2dc
walid hamdy44 <walidhamdy314@gmail.com>
2024-06-28 16:54:57 +0300

    n

--- app/(DashBoard)/(Routes)/dashboard/page.tsx
+++ app/(DashBoard)/(Routes)/dashboard/page.tsx
@@ -1,3 +1,15 @@
+import CoursesDashboard from "./_components/CoursesDashboard";
+import UserInfo from "./_components/UserInfo";
+
 export default function DashBoard() {
-  return <div>sf</div>;
+  return (
+    <div className="p-4 md:p-6">
+      <div>
+        <UserInfo />
+      </div>
+      <div className="w-full">
+        <CoursesDashboard />
+      </div>
+    </div>
+  );
 }
